13374728f7fed5e5be1212f69bf4c46a7353c984
SolBena <sol.benavides95@gmail.com>
2022-02-12 22:41:23 -0300

    Subo trabajo practico

---
+++ front/src/components/GenresInDb.js
@@ -0,0 +1,75 @@
+import React, {Component} from 'react';
+import Genre  from './Genre';
+import axios from "axios";
+
+/* let genres = [
+    {genre: 'Acción'},
+    {genre: 'Animación'},
+    {genre: 'Aventura'},
+    {genre: 'Ciencia Ficción'},
+    {genre: 'Comedia'},
+    {genre: 'Documental'},
+    {genre: 'Drama'},
+    {genre: 'Fantasia'},
+    {genre: 'Infantiles'},
+    {genre: 'Musical'}
+] */
+
+class GenresInDb extends Component{
+
+    constructor(){
+
+        super()
+
+        this.state = {
+            genres: "",
+            bg: false
+        }
+    }
+
+    componentDidMount(){
+
+        axios("http://localhost:3001/api/genres")
+        .then(response=>{
+            console.log(response);
+            this.setState({genres: response.data.genres})
+        })
+        .catch(error=>{
+            console.log(error);
+        })
+    }
+
+    mouseOverHandler(){
+        console.log("mouseOver");
+        this.setState({bg: !this.state.bg})
+    }
+
+    render(){
+        
+        return (
+            <>
+                    {/*<!-- Categories in DB -->*/}
+                    <div className="col-lg-6 mb-4">						
+                        <div className="card shadow mb-4">
+                            <div className="card-header py-3">
+                                <h6 onMouseOver={() => this.mouseOverHandler()} className="m-0 font-weight-bold text-gray-800">Genres in Data Base</h6>
+                            </div>
+                            <div className={this.state.bg ? "card-body bg-secondary" : "card-body"}>
+                                <div className="row">
+                                {
+                                this.state.genres !== "" ? this.state.genres.map((genre,index)=>{
+                                        return  <Genre  genre={genre.name}  key={index} />
+                                    })
+                                : <h1>Cargando....</h1>}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+               
+            </>
+        )
+    }
+    
+
+}
+export default GenresInDb;
